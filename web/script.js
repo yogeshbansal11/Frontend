bc5bfa0cbf02741df51947dbc962c0a528738435
yogesh bansal <bansalindia850@gmail.com>
2024-08-06 22:18:42 +0530

    script

---
+++ web/script.js
@@ -0,0 +1,15 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const Close = document.getElementById('Close');
+
+if(bar){
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if(Close){
+    Close.addEventListener('click', () => {
+       nav.classList.remove('active');
+    })
+}
